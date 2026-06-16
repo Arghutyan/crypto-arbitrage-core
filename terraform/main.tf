5e352f1568d1cd6530ce9341e6535a4b416f1383
Arghutyan <292590332+Arghutyan@users.noreply.github.com>
2026-06-16 04:15:25 +0400

    fix: remove invalid character from main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -145,7 +145,3 @@ output "dynamodb_table_name" {
   description = "Name of the DynamoDB table for Terraform state locking"
   value       = aws_dynamodb_table.terraform_locks.name
 }
-
-
-
-№test ci
